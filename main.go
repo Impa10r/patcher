c27da04c4c64ec8d5a417c060150412e2ec4f502
Impa10r <impalor@pm.me>
2025-02-27 10:58:16 +0100

    initial

---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+const MAX_FILE_SIZE = 10_000_000
+
+type Differences struct {
+	Offset int
+	Length int
+	Bytes  []byte
+}
+
+func findDifferences(src, dst string) ([]Differences, error) {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return nil, err
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Open(dst)
+	if err != nil {
+		return nil, err
+	}
+	defer destinationFile.Close()
+
+	var diffs []Differences
+	bufferSrc := make([]byte, MAX_FILE_SIZE)
+	bufferDst := make([]byte, MAX_FILE_SIZE)
+
+	nSrc, errSrc := sourceFile.Read(bufferSrc)
+	if errSrc != nil && errSrc != io.EOF {
+		return nil, errSrc
+	}
+
+	nDst, errDst := destinationFile.Read(bufferDst)
+	if errDst != nil && errDst != io.EOF {
+		return nil, errDst
+	}
+
+	minLen := min(nSrc, nDst)
+	differencesFound := false
+	var bytes []byte
+
+	for i := 0; i < minLen; i++ {
+		if bufferSrc[i] == bufferDst[i] {
+			if differencesFound {
+				// save patch
+				diffs = append(diffs, Differences{
+					Offset: i,
+					Length: len(bytes),
+					Bytes:  bytes,
+				})
+				// reset
+				bytes = []byte{}
+			}
+			differencesFound = false
+		} else {
+			bytes = append(bytes, bufferDst[i])
+			differencesFound = true
+		}
+	}
+
+	return diffs, nil
+}
+
+func applyDifferences(target string, diffs []Differences) error {
+	file, err := os.OpenFile(target, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for _, diff := range diffs {
+		if _, err := file.Seek(int64(diff.Offset), io.SeekStart); err != nil {
+			return err
+		}
+		if _, err := file.Write(diff.Bytes); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	original := "original.bin"
+	modded := "modded.bin"
+	target := "target.bin"
+
+	differences, err := findDifferences(original, modded)
+	if err != nil {
+		log.Fatalf("Error finding differences: %v", err)
+	}
+
+	for _, diff := range differences {
+		log.Printf("Difference at offset %d, length %d, bytes: %v", diff.Offset, diff.Length, diff.Bytes)
+	}
+
+	if err := applyDifferences(target, differences); err != nil {
+		log.Fatalf("Error applying differences: %v", err)
+	}
+
+	log.Println("Differences applied successfully!")
+}
